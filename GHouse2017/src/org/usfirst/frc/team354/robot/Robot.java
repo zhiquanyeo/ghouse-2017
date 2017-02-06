f82a41d78fcdbf0402d1cbda573a5717708aa8e9
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2017-02-05 22:46:07 -0500

    Start CameraServer on robot boot up

--- GHouse2017/src/org/usfirst/frc/team354/robot/Robot.java
+++ GHouse2017/src/org/usfirst/frc/team354/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team354.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -41,6 +42,9 @@ public class Robot extends IterativeRobot {
 		oi = new OI();
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+		
+		// Set up CameraServer
+		CameraServer.getInstance().startAutomaticCapture();
 	}
 
 	/**
