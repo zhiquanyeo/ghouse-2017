7cc81653eecc4399f7f93c1fa0014e051fa56ac8
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2017-02-18 19:25:08 +0530

    Make ToggleShooter a one-shot command

--- GHouse2017/src/org/usfirst/frc/team354/robot/commands/ToggleShooter.java
+++ GHouse2017/src/org/usfirst/frc/team354/robot/commands/ToggleShooter.java
@@ -12,21 +12,21 @@ public class ToggleShooter extends Command {
 	
 	 // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.shooter.toggleShooting();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.shooter.toggleShooting();
+    	// do nothing
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.intake.stop();
     }
 
     // Called when another command which requires one or more of the same
